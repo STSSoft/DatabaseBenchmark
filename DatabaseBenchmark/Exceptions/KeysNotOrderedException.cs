26d8b82c30202f75c920d8be56ec6258acb273da
ChrisDimitrov <krisko92@gmail.com>
2015-03-24 13:08:06 +0200

    Modification to the KeysNotFound exception class.

--- DatabaseBenchmark/Exceptions/KeysNotOrderedException.cs
+++ DatabaseBenchmark/Exceptions/KeysNotOrderedException.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace DatabaseBenchmark.Exceptions
 {
+    /// <summary>
+    /// Represents an error that occurs when a database does not read records ordered by key.
+    /// </summary>
     public class KeysNotOrderedException : Exception
     {
+        private string message;
+        private Exception inner;
+
+        public KeysNotOrderedException()
+        {
+        }
+
+        public KeysNotOrderedException(string message)
+        {
+            this.message = message;
+        }
+
+        public KeysNotOrderedException(string message, Exception inner)
+        {
+            this.message = message;
+            this.inner = inner;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return message;
+            }
+        }
     }
 }
